a74447478f32bb2311f267fc7b05d0fdb86d38de
SriBarathSelva <115051231+SriBarathSelva@users.noreply.github.com>
2024-10-20 20:03:27 +0530

    server.js

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const port = process.env.PORT || 5000;
 
 connectDb();
 
-app.use(cors());  
+app.use(cors
+    ({
+  origin: 'https://resume-builder-deploy-1.onrender.com', // Frontend domain
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true, // If you need to send cookies or authentication tokens
+})
+);  
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
